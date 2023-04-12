eea34433d03d7952b3e3c4ab70acaf7f5f8ad58d
SungBin <djsbpark@naver.com>
2023-04-12 18:28:13 +0900

    * 로그인 쿠키 없을시 로그인 페이지 이동 처리 로직 수정

--- src/utils/util.tsx
+++ src/utils/util.tsx
@@ -5,10 +5,11 @@ const ENDPOINT = 'http://192.168.0.58:8080';
 export async function request<TResponse>(
     url: string,
     config?: RequestInit,
+    noCheckAuth?: boolean
 ): Promise<Response> {
     const response = await fetch(ENDPOINT+url, config);
     const isAuthenticated = !!Cookies.get('isLogin');
-    if(isAuthenticated === null || !isAuthenticated){
+    if((isAuthenticated === null || !isAuthenticated) && !noCheckAuth){
         window.location.href = '/login'
     }
     return response;
